a49e0f4964460f4af45de4746617a645cc6e6a64
Dhaval Patel <Dhaval Patel@LAPTOP-84LTHVUQ>
2022-01-06 12:05:13 +0530

    First Commit.

---
+++ onlineshopingapp1/src/main/java/com/cg/project1/Onlineshopingapp1Application.java
@@ -0,0 +1,13 @@
+package com.cg.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onlineshopingapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onlineshopingapp1Application.class, args);
+	}
+
+}
